6702763bf01b1ec07c3872e5ff2fc4624db2769a
MarcusOme <omezzollimarco@gmail.com>
2024-09-16 17:05:31 +0200

    Lezione del 20/09/2024
    Aggiornato con override

--- Ereditarieta.py
+++ Ereditarieta.py
@@ -11,13 +11,16 @@ class Figlio(Padre): #in questa riga dichiaro che la classe Figlio eredita da Pa
         self.scuola = scuola
     def info_scuola(self):
         print(f"Sono iscritto alla scuola {self.scuola}")
+    def saluta(self): #override del metodo saluta
+        super().saluta()
+        print(f"Sono uno studente della scuola {self.scuola}")
 
 p = Padre("Mario") #creo l'istanza di padre
 f = Figlio("Carlo", "Scuola Martino Rossi") #creo l'istanza del figlio
 
 f.saluta() #il figlio può usare il metodo del padre
 f.info_scuola() #il figlio può usare i suoi metodi
-
+f.saluta() #in questo caso saluto con il metodo che ha eseguito override
 #ERRORE!
 p.info_scuola() #il metofo info_scuola() non fa parte della classe del padre e non può usarlo
 
